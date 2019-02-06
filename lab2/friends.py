d59a1c8ee44ea7a520ff52588f9bd9130a9e36e6
Sithu Aung <sithu_aung@intuit.com>
2019-02-05 20:24:27 -0800

    add print calls

--- lab2/friends.py
+++ lab2/friends.py
@@ -5,7 +5,8 @@ def median_num_friends(x):
     # TODO
 
 num_friends=[100, 49, 41, 40, 25, 10, 5, 4, 7, 20, 60]
-mean_num_friends(num_friends)
-median_num_friends(num_friends)
+print("mean={}".format(mean_num_friends(num_friends))
+
+print("median={}".format(median_num_friends(num_friends))
 
 
