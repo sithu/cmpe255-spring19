8b71f34206458bc5166246bafc503eb2961d5994
Sithu Aung <sithu_aung@intuit.com>
2019-02-05 20:22:28 -0800

    add test data

--- lab2/friends.py
+++ lab2/friends.py
@@ -1,51 +1,11 @@
-users =[
-    { "id":0, "name": "Hero" },
-    { "id":1, "name": "Dunn" },
-    { "id":2, "name": "Sue" },
-    { "id":3, "name": "Chi" },
-    { "id":4, "name": "Thor" },
-    { "id":5, "name": "Clive" },
-    { "id":6, "name": "Hicks" },
-    { "id":7, "name": "Devin" },
-    { "id":8, "name": "Kate" },
-    { "id":9, "name": "Klein" }    
-]
-
-friendship = [
-    (0, 1),
-    (0, 2),
-    (1, 2),
-    (1, 3),
-    (2, 3),
-    (3, 4),
-    (4, 5),
-    (5, 6),
-    (6, 7),
-    (6, 8),
-    (7, 8),
-    (8, 9)
-]
-
-def num_friends(user):
-    '''
-    Find number of friends for a given user
-    '''
-    # TODO
-    return 3
-
-def sort_by_num_friends():
-    '''README.md
-    Sort from "most friends" to "least friends"
-    '''
-    print("{} has {} friends".format("Dunn", num_friends("x") + 1))
-    print("{} has {} friends".format("Kate", num_friends("x")))
-    
-
-def mean_num_friends(user_id):
+def mean_num_friends(x):
     # TODO
 
-def median_num_friends(user_id):
+def median_num_friends(x):
     # TODO
 
+num_friends=[100, 49, 41, 40, 25, 10, 5, 4, 7, 20, 60]
+mean_num_friends(num_friends)
+median_num_friends(num_friends)
 
 
