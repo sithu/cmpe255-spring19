be7b2a935f6330369e14956546d1a9ef3058e7f7
Sithu Aung <sithu_aung@intuit.com>
2019-02-05 20:12:56 -0800

    add lab2

---
+++ lab2/README.md
@@ -0,0 +1,11 @@
+# Lab 2
+
+1. Complete all TODOs sections in friends.py
+
+
+2. In normal_df.py, implement the [normal distribution](https://en.wikipedia.org/wiki/Normal_distribution).
+
+
+
+
+
---
+++ lab2/normal_df.py
@@ -0,0 +1,12 @@
+def normal_pdf(x, mu=0, sigma=1):
+    # TODO
+    # hits: math.exp
+
+from matplotlib import pyplot as plt
+xs = [x / 10.0 for x in range(-50, 50)]
+plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
+plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], '-', label='mu=0,sigma=2')
+plt.plot(xs, [normal_pdf(x, sigma=0.5) for x in xs], '-', label='mu=0,sigma=0.5')
+plt.plot(xs, [normal_pdf(x, sigma=-1) for x in xs], '-', label='mu=0,sigma=-1')
+plt.legend()
+plt.show()
