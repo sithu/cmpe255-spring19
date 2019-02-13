d4d6165ac44a76383368b5c381b37e4442dc5749
Sithu Aung <sithu_aung@intuit.com>
2019-02-12 19:16:26 -0800

    add lab 3

---
+++ lab3/README.md
@@ -0,0 +1,26 @@
+# Lab 3
+
+In this lab, you will be doing a sentiment analysis on Twitter tweets by building a linear classifier. The tweet data is already labeled as negative and positive tweets. E.g. pos_tweets.txt.
+
+## Install PIP Dependency
+
+```
+pip3 install scikit-learn==0.19.0
+pip3 install jupyter==1.0.0
+```
+
+## Run Jupyter
+
+```
+jupyter notebook
+```
+
+## Expected Output
+
+```
+::neg::"Noooooooo!!! There are clips missing on youtube "
+...
+
+
+Accuracy=x.xxxxxxxxxx
+```
---
+++ lab3/tweet_classifier.py
@@ -0,0 +1,65 @@
+def load_data():
+    data = []
+    data_labels = []
+    with open("./pos_tweets.txt") as f:
+        for i in f: 
+            data.append(i) 
+            data_labels.append('pos')
+
+    with open("./neg_tweets.txt") as f:
+        for i in f: 
+            data.append(i)
+            data_labels.append('neg')
+
+    return data, data_labels
+
+def transform_to_features(data):
+    from sklearn.feature_extraction.text import CountVectorizer
+    vectorizer = CountVectorizer(
+        analyzer = 'word',
+        lowercase = False,
+    )
+    features = vectorizer.fit_transform(
+        data
+    )
+    features_nd = features.toarray()
+    return features_nd
+
+def train_then_build_model(data_labels, features_nd):
+    from sklearn.cross_validation import train_test_split
+    # TODO : set training % to 80%.
+    X_train, X_test, y_train, y_test  = train_test_split(
+        features_nd, 
+        data_labels,
+        train_size=0.80, 
+        random_state=1234)
+
+    from sklearn.linear_model import LogisticRegression
+    log_model = LogisticRegression()
+
+    log_model = log_model.fit(X=X_train, y=y_train)
+    y_pred = log_model.predict(X_test)
+
+    # print first 10th prediction in this format:
+    # ::{prediction}::{tweet}
+    # TODO
+
+    import random
+    j = random.randint(0,len(X_test)-7)
+    for i in range(j,j+7):
+        print(y_pred[0])
+        ind = features_nd.tolist().index(X_test[i].tolist())
+        print(data[ind].strip())
+    
+    # print accuracy
+    from sklearn.metrics import accuracy_score
+    # TODO
+    print("Accuracy={}".format('fix-me'))
+    
+def process():
+    data, data_labels = load_data()
+    features_nd = transform_to_features(data)
+    train_then_build_model(data_labels, features_nd)
+
+
+process()
