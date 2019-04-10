37cbcdadb787e1dfd444b7f87868dafad8fa8597
Sithu Aung <sithu_aung@intuit.com>
2019-04-09 19:57:22 -0700

    fix lab 7 baseline

--- lab7/svm.py
+++ lab7/svm.py
@@ -2,42 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd  
 
-# download data set: https://drive.google.com/file/d/13nw-uRXPY8XIZQxKRNZ3yYlho-CYm_Qt/view
-# info: https://archive.ics.uci.edu/ml/datasets/banknote+authentication
+def linear_svm():
+    # download data set: https://drive.google.com/file/d/13nw-uRXPY8XIZQxKRNZ3yYlho-CYm_Qt/view
+    # info: https://archive.ics.uci.edu/ml/datasets/banknote+authentication
 
-# load data
-bankdata = pd.read_csv("FIX_ME_PATH/bill_authentication.csv")  
+    # load data
+    bankdata = pd.read_csv("/tmp/bill_authentication.csv")  
 
-# see the data
-bankdata.shape  
+    # see the data
+    bankdata.shape  
 
-# see head
-bankdata.head()  
+    # see head
+    bankdata.head()  
 
-# data processing
-X = bankdata.drop('Class', axis=1)  
-y = bankdata['Class']  
+    # data processing
+    X = bankdata.drop('Class', axis=1)  
+    y = bankdata['Class']  
 
-from sklearn.model_selection import train_test_split  
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)  
+    from sklearn.model_selection import train_test_split  
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)  
 
-# train the SVM
-from sklearn.svm import SVC  
-svclassifier = SVC(kernel='linear')  
-svclassifier.fit(X_train, y_train)  
+    # train the SVM
+    from sklearn.svm import SVC  
+    svclassifier = SVC(kernel='linear')  
+    svclassifier.fit(X_train, y_train)  
 
-# predictions
-y_pred = svclassifier.predict(X_test)  
+    # predictions
+    y_pred = svclassifier.predict(X_test)  
 
-# Evaluate model
-from sklearn.metrics import classification_report, confusion_matrix  
-print(confusion_matrix(y_test,y_pred))  
-print(classification_report(y_test,y_pred))  
+    # Evaluate model
+    from sklearn.metrics import classification_report, confusion_matrix  
+    print(confusion_matrix(y_test,y_pred))  
+    print(classification_report(y_test,y_pred))  
 
-# TODO output
-"""
-
-"""
 
 # Iris dataset  https://archive.ics.uci.edu/ml/datasets/iris4
 def import_iris():
@@ -57,5 +54,22 @@ def import_iris():
     from sklearn.model_selection import train_test_split  
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)  
 
-    # TODO: Evaluates perfomance of Polynomial Kernel, Gaussian Kernel, and Sigmoid Kernel.
-    
+def polynomial_kernel():
+    # TODO
+    # Trains, predicts and evaluates the model
+
+def gaussian_kernel():
+    # TODO
+    # Trains, predicts and evaluates the model
+
+def sigmoid_kernel():
+    # TODO
+    # Trains, predicts and evaluates the model
+
+def test():
+    import_iris()
+    polynomial_kernel()
+    gaussian_kernel()
+    sigmoid_kernel()
+
+test()
